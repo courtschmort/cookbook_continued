4eb0f261617610ab9a369ca81b88805e9201d520
Student <example@example.com>
2020-01-16 11:27:07 -0800

    nest recipes in tags and tags in recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'recipes#index'
-  resources :recipes
-  resources :tags
+  resources :recipes do
+    resources :tags
+  end
+  resources :tags do
+    resources :recipes
+  end
 end
