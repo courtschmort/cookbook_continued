2b5636f709c052b7a55d896d59a5df8ffb3027ac
Student <example@example.com>
2020-01-16 08:12:26 -0800

    Add validation and callback, specs passing

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,14 @@
 class Recipe < ApplicationRecord
+  validates :title, presence: true
+  validates_length_of :title, maximum: 100
+
   has_many :recipe_tags
   has_many :tags, :through => :recipe_tags
+
+  before_save(:titleize_recipe)
+
+  private
+  def titleize_recipe
+    self.title = self.title.titleize
+  end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 describe Recipe do
   it { should have_many(:tags).through(:recipe_tags) }
+  it { should validate_presence_of :title }
+  it { should validate_length_of(:title).is_at_most(100) }
+
+  it("titleizes the name of an recipe") do
+    recipe = Recipe.create({title: "pad see ew"})
+    expect(recipe.title()).to(eq("Pad See Ew"))
+  end
 end
