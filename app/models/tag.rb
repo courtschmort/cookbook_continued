4f66227c9f23247775ddbd1caa35fbb6e19582ee
Student <example@example.com>
2020-01-16 09:11:21 -0800

    add tag form layout and TagsContoller

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,13 @@
 class Tag < ApplicationRecord
+  validates :categories, presence: true
+
   has_many :recipe_tags
   has_many :recipes, :through => :recipe_tags
+
+  before_save(:downcase_tag)
+
+  private
+  def downcase_tag
+    self.categories = self.categories.downcase
+  end
 end
